bbb378e52b0ee74431b09cbd14ef713d616a0a42
Andrew floatrx <ifloatrx@gmail.com>
2024-06-20 19:38:39 +0300

    fix eslint config errors

--- app/error.tsx
+++ app/error.tsx
@@ -1,25 +1,20 @@
 'use client';
 
+import { Button } from '@nextui-org/button';
 import { useEffect } from 'react';
+
+import { Heading } from '@/components/ui/layout/headings/Heading';
 
 export default function Error({ error, reset }: { error: Error; reset: () => void }) {
   useEffect(() => {
     // Log the error to an error reporting service
-    /* eslint-disable no-console */
     console.error(error);
   }, [error]);
 
   return (
     <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
+      <Heading>Something went wrong!</Heading>
+      <Button onClick={reset}>Try again</Button>
     </div>
   );
 }
