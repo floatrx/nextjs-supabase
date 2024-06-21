b08fa8b6e90ec55dffc131f1944cb957071f5084
Andrew floatrx <ifloatrx@gmail.com>
2024-06-22 02:34:10 +0300

    fix lib/string: upperFirst

--- lib/string.ts
+++ lib/string.ts
@@ -2,8 +2,8 @@
  * Capitalize the first letter of a string
  * @param str
  */
-export const upperFirst = (str: string): string => {
-  return str.charAt(0).toUpperCase() + str.slice(1);
+export const upperFirst = (str?: string | null): string => {
+  return str ? str.charAt(0).toUpperCase() + str.slice(1) : '';
 };
 
 /**
