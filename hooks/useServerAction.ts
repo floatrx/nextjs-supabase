ad9b2cefd22e7cbaf450e7dcbb740d8339c28655
Andrew floatrx <ifloatrx@gmail.com>
2024-06-28 20:26:23 +0300

    update useServerAction hook

--- hooks/useServerAction.ts
+++ hooks/useServerAction.ts
@@ -2,20 +2,32 @@ import { useState } from 'react';
 
 type AsyncFn = (...args: any) => Promise<any>;
 
+/**
+ * Use this hook to execute server actions in the client side
+ * It will handle loading, error and data states...
+ * @param action
+ * @example
+ *    export const DeleteButton = ({id}) => {
+ *      const { loading, execute } = useServerAction(deleteNote);
+ *      return (
+ *        <Button isLoading={loading} onClick={() => execute(id)}>Delete</Button>
+ *      );
+ *    };
+ */
 export const useServerAction = <T extends AsyncFn>(action: T) => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<Error | null>(null);
-  const [data, setData] = useState<Awaited<ReturnType<T>> | null>(null);
+  const [response, setResponse] = useState<Awaited<ReturnType<T>> | null>(null);
 
   const execute = async (...args: Parameters<T>) => {
     setLoading(true);
     setError(null);
-    setData(null);
+    setResponse(null);
 
     try {
       const res = await action(...args);
 
-      setData(res);
+      setResponse(res);
 
       return res as Awaited<ReturnType<T>>;
     } catch (e) {
@@ -27,7 +39,7 @@ export const useServerAction = <T extends AsyncFn>(action: T) => {
   };
 
   return {
-    data,
+    response,
     loading,
     error,
     execute,
