3347ddb75892468e48fb1ca705b6b3772413eac9
Andrew floatrx <ifloatrx@gmail.com>
2024-07-18 16:54:07 +0300

    remove deprecated useServerAction

--- features/note/components/DeleteNoteButton.tsx
+++ features/note/components/DeleteNoteButton.tsx
@@ -4,15 +4,15 @@ import type { TNote } from '@/types/note';
 
 import { Button } from '@nextui-org/button';
 import { Trash2 } from 'lucide-react';
+import { useServerAction } from 'zsa-react';
 
 import { deleteNote } from '@/features/note/actions/deleteNote';
-import { useServerAction_deprecated } from '@/hooks/useServerAction_deprecated';
 
 export const DeleteNoteButton = (props: Pick<TNote, 'id'>) => {
-  const { loading, execute } = useServerAction_deprecated(deleteNote);
+  const { isPending, execute } = useServerAction(deleteNote);
 
   return (
-    <Button isIconOnly isLoading={loading} size="sm" variant="ghost" onClick={() => execute(props.id)}>
+    <Button isIconOnly isLoading={isPending} size="sm" variant="ghost" onClick={() => execute(props.id)}>
       <Trash2 size={15} />
     </Button>
   );
