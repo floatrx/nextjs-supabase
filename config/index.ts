d62957ea1f26f4627b6569064d091cf8c4161fb8
Andrew floatrx <ifloatrx@gmail.com>
2026-02-11 01:49:32 +0200

    fix: use NEXT_PUBLIC_SITE_URL for OAuth redirects

--- config/index.ts
+++ config/index.ts
@@ -1,1 +1,2 @@
-export const baseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000';
+export const baseUrl =
+  process.env.NEXT_PUBLIC_SITE_URL || (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000');
